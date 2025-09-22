5a337cd10cf58ccaebec68a9722385282f3ef416
jeffersondlmatta <jeffersondlmatta@gmail.com>
2025-09-22 20:26:23 -0300

    criar metodos dinamicos

--- pessoa/src/main/java/br/com/api/pessoa/controle/PessoaControle.java
+++ pessoa/src/main/java/br/com/api/pessoa/controle/PessoaControle.java
@@ -1,6 +1,7 @@
 package br.com.api.pessoa.controle;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // especifica que essa classe é um controlador REST
@@ -8,6 +9,11 @@ public class PessoaControle {
 
     @GetMapping("/mensagem") // mapeia requisições GET para o método mensagem() 
     public String mensagem(){
-        return "Helo World!";
+        return "spring boot API pessoa";
     }
-}
+
+    @GetMapping("/apresentacao/{nome}") //trabalhar com variáveis de caminho 
+    public String apresentaacao(@PathVariable String nome){
+        return "Olá, eu sou " + nome + "!";
+    }
+}   
